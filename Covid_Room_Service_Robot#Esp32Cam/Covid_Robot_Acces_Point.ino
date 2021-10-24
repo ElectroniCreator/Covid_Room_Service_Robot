738b8adc47ee98792dedafe88484f21cdb9bf67e
Youssef Allagui <91192200+ElectroniCreator@users.noreply.github.com>
2021-10-24 15:01:19 +0100

    Covid_Room_Service_Robot

--- Covid_Room_Service_Robot#Esp32Cam/Covid_Robot_Acces_Point.ino
+++ Covid_Room_Service_Robot#Esp32Cam/Covid_Robot_Acces_Point.ino
@@ -35,21 +35,16 @@ const int MotPin3 = 15;
 
 void initMotors()
 {
-  ledcSetup(3, 2000, 8); // 2000 hz PWM, 8-bit resolution
-  ledcSetup(4, 2000, 8); // 2000 hz PWM, 8-bit resolution
-  ledcSetup(5, 2000, 8); // 2000 hz PWM, 8-bit resolution
-  ledcSetup(6, 2000, 8); // 2000 hz PWM, 8-bit resolution
+  ledcSetup(3, 2000, 8);
+  ledcSetup(4, 2000, 8); 
+  ledcSetup(5, 2000, 8); 
+  ledcSetup(6, 2000, 8); 
   ledcAttachPin(MotPin0, 3);
   ledcAttachPin(MotPin1, 4);
   ledcAttachPin(MotPin2, 5);
   ledcAttachPin(MotPin3, 6);
 }
 
-const int ServoPin = 2;
-void initServo(){
-  ledcSetup(8, 50, 16); // 50 hz PWM, 16-bit resolution, range from 3250 to 6500.
-  ledcAttachPin(ServoPin, 8);
-}
 
 void setup(){
   WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); // prevent brownouts by silencing them
@@ -79,7 +74,7 @@ void setup(){
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
   config.pixel_format = PIXFORMAT_JPEG;
-  //init with high specs to pre-allocate larger buffers
+  
   if (psramFound()){
     config.frame_size = FRAMESIZE_QVGA;
     config.jpeg_quality = 10;
@@ -105,7 +100,6 @@ void setup(){
 
   // Remote Control Car
   initMotors();
-  initServo();
 
   ledcSetup(7, 5000, 8);
   ledcAttachPin(4, 7);  //pin4 is LED
@@ -113,7 +107,7 @@ void setup(){
   WiFi.softAP(ssid, password);
   IPAddress miIP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
-  Serial.println(miIP); //probar 192.168.4.1
+  Serial.println(miIP); //LOCALHOST   is   192.168.4.1
 
   startCameraServer();
 
@@ -126,7 +120,7 @@ void setup(){
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+ 
   delay(1000);
   Serial.printf("RSSi: %ld dBm\n", WiFi.RSSI());
 }
